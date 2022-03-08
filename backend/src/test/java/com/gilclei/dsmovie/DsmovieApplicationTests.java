2467bf520a0e23809f764eb8adc662ae0e7b6a52
gil <gilclei@gmail.com>
2022-03-07 22:19:15 -0300

    first commit

---
+++ backend/src/test/java/com/gilclei/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilclei.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
