2a0b187ca70824fd1f7b569d29a563ffdf9e5a12
gil <gilclei@gmail.com>
2022-03-11 22:52:36 -0300

    useParams

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
                     <a href="https://github.com/gilcleis" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/gilclei</p>
                         </div>
                     </a>
                 </div>
